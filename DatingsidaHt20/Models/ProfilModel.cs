39d46bea946637107d774cebb5645edbf083992d
Carolin Nygren <nygren.c@gmail.com>
2020-12-21 11:14:49 +0100

    Create ProfilModel.cs

---
+++ DatingsidaHt20/Models/ProfilModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class ProfilModel
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public int Age { get; set; }
+    [Required]
+    public string Gender { get; set; }
+    [Required]
+    public string Sexuality { get; set; }
+    [Required]
+    public string ImageFilepath { get; set; }
+    [Required]
+    public string Presentation { get; set; }
+    public bool IsActive { get; set; }
+
+    public ProfilModel()
+	{
+	}
+}
